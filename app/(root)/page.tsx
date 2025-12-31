16bab2956e04b30e11bbc4a93206968bc73569bf
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-31 23:09:24 +0200

    update HomePage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,13 @@
+import ProductList from "@/components/shared/product/ProductList"
+import sampleData from "@/db/sample-data"
 
 
 
 function HomePage() {
   return (
-    <div >HomePage</div>
+    < >
+      <ProductList data={sampleData.products} title="Newest Arrivals" limit={4} />
+    </>
   )
 }
 
