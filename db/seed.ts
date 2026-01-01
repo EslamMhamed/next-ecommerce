af7795dbc2a20661873c61ba2723b60ecb58de63
Eslam AbdelWahab <mohaeslam22@gmail.com>
2026-01-01 11:41:16 +0200

    create seed , Instantiate Prisma Client

---
+++ db/seed.ts
@@ -0,0 +1,11 @@
+import sampleData from "./sample-data";
+import { prisma } from "@/lib/prisma";
+
+async function main() {
+  await prisma.product.deleteMany();
+  await prisma.product.createMany({ data: sampleData.products });
+
+  console.log("Database seeded successfully!");
+}
+
+main();
