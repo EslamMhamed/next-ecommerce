4b2c0d60202db1043d1a569acebe2d51f4c47379
Eslam AbdelWahab <mohaeslam22@gmail.com>
2026-01-10 19:06:07 +0200

    shpping address and form

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -3,6 +3,8 @@ import { getMyCart } from "@/lib/actions/cart.action"
 import { getUserById } from "@/lib/actions/user.action"
 import { Metadata } from "next"
 import { redirect } from "next/navigation"
+import ShippingAddressForm from "./ShippingAddressForm"
+import { ShippingAddress } from "@/types"
 
 export const metadata: Metadata = {
     title : "Shipping Address"
@@ -19,7 +21,9 @@ async function page() {
     const user = await getUserById(userId)
     
   return (
-    <div>page</div>
+    <>
+        <ShippingAddressForm address={user.address as ShippingAddress} />
+    </>
   )
 }
 
