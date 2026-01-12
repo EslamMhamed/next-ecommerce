6dda5ce19c4bdb58611afff7eb9623373b2c0ecb
Eslam AbdelWahab <mohaeslam22@gmail.com>
2026-01-12 10:56:49 +0200

    create payment method page

---
+++ app/(root)/payment-method/page.tsx
@@ -0,0 +1,25 @@
+import { auth } from "@/auth"
+import { getUserById } from "@/lib/actions/user.action"
+import PaymentMethodForm from "./PaymentMethodForm"
+
+export const metadata = {
+    title: "Select Payment Method"
+} 
+
+
+async function PaymentMethodPage() {
+    const session = await auth()
+    const userId = session?.user?.id
+
+    if(!userId) throw new Error("User not found")
+    
+    const user = await getUserById(userId)
+
+  return (
+    <>
+        <PaymentMethodForm preferredPaymentMethod={user.paymentMethod} />
+    </>
+  )
+}
+
+export default PaymentMethodPage
