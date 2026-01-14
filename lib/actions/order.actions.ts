610dd200004c89b48fcae979e3d6f904b690690e
Eslam AbdelWahab <mohaeslam22@gmail.com>
2026-01-14 23:54:27 +0200

    get my order action

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -11,6 +11,7 @@ import { CartItem, PaymentResult } from "@/types";
 import { paypal } from "../paypal";
 import { ca, tr } from "zod/v4/locales";
 import { revalidatePath } from "next/cache";
+import { PAGE_SIZE } from "../constants";
 
 // Create order and create the order items
 export async function createOrder() {
@@ -207,5 +208,18 @@ async function updateOrderToPaid({orderId, paymentResult}: {orderId: string, pay
     })
 
     if(!updatedOrder)throw new Error("Order not found")
+
+}
+
+// Get user's orders
+export async function getMyOrders({limit = PAGE_SIZE, page}:{limit?: number, page:number}){
+  const session =  await auth()
+  if(!session) throw new Error("User is not authorized")
+     
+  const data = await prisma.order.findMany({where: {userId:session.user?.id}, orderBy: {createdAt: "desc"}, take: limit, skip: page -1 * limit  })
+
+  const dataCount = await prisma.order.count({where: {userId: session.user?.id}})
+
+  return {data, totalPages: Math.ceil(dataCount / limit)}
 
 }
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -27,3 +27,5 @@ export const shippingAddressDefaltValues = {
 export const PAYMENT_METHODS = process.env.PAYMENT_METHODS ? process.env.PAYMENT_METHODS.split(", ") : ["PayPal", "Stripe", "CashOnDelivery"] 
 
 export const DEFAULT_PAYMENT_METHOD = process.env.DEFAULT_PAYMENT_METHOD || "Paypal"
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 2
