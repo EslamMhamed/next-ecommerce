229ad6c0d2446b62de3efd6da38fcd485c89b563
Eslam AbdelWahab <mohaeslam22@gmail.com>
2026-01-17 21:24:30 +0200

    delete order action

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -272,3 +272,15 @@ export async function getAllOrder({limit=PAGE_SIZE, page}: {limit?: number,page:
   return {data, totalPages : Math.ceil(dataCount/limit)}
 
 }
+
+// Delete an order
+export async function deleteOrder(id: string){
+  try {
+    await prisma.order.delete({where:{id}})
+    revalidatePath("/admin/ordes")
+
+    return {success: true , message: "Order deleted successfully"}
+  } catch (error) {
+    return {success: false, message: formatError(error)}
+  }
+}
