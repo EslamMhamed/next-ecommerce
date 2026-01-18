ebe873a22999b7c93247e35587c24b59049e6e6d
Eslam AbdelWahab <mohaeslam22@gmail.com>
2026-01-18 13:55:26 +0200

    create and update actions

--- lib/actions/procdut.action.ts
+++ lib/actions/procdut.action.ts
@@ -1,45 +1,86 @@
-"use server"
+"use server";
 
-import { revalidatePath } from "next/cache"
-import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants"
-import { prisma } from "../prisma"
-import { convertToPlainObject, formatError } from "../utils"
+import { revalidatePath } from "next/cache";
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
+import { prisma } from "../prisma";
+import { convertToPlainObject, formatError } from "../utils";
+import z from "zod";
+import { insertProductschema, updateProductSchema } from "../constants/validators";
 
 // Get latest products
 export async function getLatestProduts() {
-    const data = await prisma.product.findMany({take: LATEST_PRODUCTS_LIMIT, orderBy: {createdAt: "desc"}})
+  const data = await prisma.product.findMany({
+    take: LATEST_PRODUCTS_LIMIT,
+    orderBy: { createdAt: "desc" },
+  });
 
-    return convertToPlainObject(data) 
+  return convertToPlainObject(data);
 }
 
 //Get single product by slug
-export async function getProductBySlug(slug:string) {
-    return prisma.product.findFirst({
-        where: {slug}
-    })
+export async function getProductBySlug(slug: string) {
+  return prisma.product.findFirst({
+    where: { slug },
+  });
 }
 
-// Get all products 
-export async function getAllProducts({query, limit= PAGE_SIZE   , page, category}: {query: string, limit?:number, page: number, category?: string}){
-    const data = await prisma.product.findMany({
-        skip: (page - 1) * limit,
+// Get all products
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category?: string;
+}) {
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
     take: limit,
-    })
+  });
 
-    const dataCount = await prisma.product.count()
+  const dataCount = await prisma.product.count();
 
-    return {data, totalPages: Math.ceil(dataCount / limit)}
+  return { data, totalPages: Math.ceil(dataCount / limit) };
 }
 
 // Delete a product
-export async function deleteProduct(id: string){
-    try {
-       const productExist = await prisma.product.findFirst({where: {id}})
-       if(!productExist) throw new Error("Product not found ")
-        await prisma.product.delete({where: {id}})
-       revalidatePath(`/admin/products`)
-       return {success: true, message: "Product deleted successfully"}
-    } catch (error) {
-        return {success : false, message: formatError(error)}
-    }
+export async function deleteProduct(id: string) {
+  try {
+    const productExist = await prisma.product.findFirst({ where: { id } });
+    if (!productExist) throw new Error("Product not found ");
+    await prisma.product.delete({ where: { id } });
+    revalidatePath(`/admin/products`);
+    return { success: true, message: "Product deleted successfully" };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// Create a product
+export async function createProduct(data: z.infer<typeof insertProductschema>) {
+  try {
+    const product = insertProductschema.parse(data);
+    await prisma.product.create({ data: product });
+    revalidatePath("/admin/prodcts");
+    return { success: true, message: "Product create successfully" };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// update a product
+export async function updateProduct(data: z.infer<typeof updateProductSchema>) {
+  try {
+    const product = updateProductSchema.parse(data);
+    const productExists = await prisma.product.findFirst({where: {id: product.id}})
+    if(!productExists) throw new Error("Product not found")
+    await prisma.product.update({where: {id: product.id}, data: product});
+    revalidatePath("/admin/prodcts");
+    return { success: true, message: "Product updated successfully" };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
 }
