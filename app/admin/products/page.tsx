895a0327de17e00af1a252b51ddb52ebd0913a0f
Eslam AbdelWahab <mohaeslam22@gmail.com>
2026-01-18 11:16:56 +0200

    display products

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,14 @@
+import Pagination from "@/components/shared/Pagination"
+import { Button } from "@/components/ui/button"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { getAllProducts } from "@/lib/actions/procdut.action"
+import { formatId, formatterCurrency } from "@/lib/utils"
+import Link from "next/link"
 
 
 async function AdminProductsPage({searchParams}: {searchParams:{page: string, query: string, category: string}}) {
 
-    const {category , page, query} = searchParams
+    const {category , page, query} = await searchParams
 
     const currentPage= Number(page) || 1
     const searchText = query || ""
@@ -11,7 +16,6 @@ async function AdminProductsPage({searchParams}: {searchParams:{page: string, qu
 
     const products = await getAllProducts({query:searchText, page:currentPage, category: selectedCategory})
 
-    console.log(products)
 
   return (
     <div className="space-y-2">
@@ -19,7 +23,46 @@ async function AdminProductsPage({searchParams}: {searchParams:{page: string, qu
             <h1 className="b2-bold">
                 Products
             </h1>
+            <Button asChild variant="default">
+                <Link href={'/admin/products/create'} >
+                Create Product</Link>
+            </Button>
         </div>
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>ID</TableHead>
+                    <TableHead>NAME</TableHead>
+                    <TableHead className="text-right">PRICE</TableHead>
+                    <TableHead>CATEGORY</TableHead>
+                    <TableHead>STOCK</TableHead>
+                    <TableHead>RATING</TableHead>
+                    <TableHead className="w-25">ACTIONS</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {products.data.map(product=> (
+                    <TableRow key={product.id}>
+                        <TableCell>
+                            {formatId(product.id)}
+                        </TableCell>
+                        <TableCell>{product.name}</TableCell>
+                        <TableCell className="text-right">{formatterCurrency(Number(product.price))}</TableCell>
+                        <TableCell>{product.category}</TableCell>
+                        <TableCell>{product.stock}</TableCell>
+                        <TableCell>{Number(product.rating)}</TableCell>
+                        <TableCell className="flex gap-1 ">
+                            <Button asChild variant="outline" size="sm">
+                                <Link href={`/admin/products/${product.id}`}>
+                                Edit</Link>
+                                {/* Delete */}
+                            </Button>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+        {products?.totalPages && products?.totalPages > 1 && <Pagination page={currentPage} totalPages={products.totalPages}  />}
     </div>
   )
 }
--- lib/actions/procdut.action.ts
+++ lib/actions/procdut.action.ts
@@ -20,9 +20,10 @@ export async function getProductBySlug(slug:string) {
 }
 
 // Get all products 
-export async function getAllProducts({query, limit= PAGE_SIZE , page, category}: {query: string, limit?:number, page: number, category?: string}){
+export async function getAllProducts({query, limit= PAGE_SIZE   , page, category}: {query: string, limit?:number, page: number, category?: string}){
     const data = await prisma.product.findMany({
-         take: limit , skip :(page -1 ) * limit 
+        skip: (page - 1) * limit,
+    take: limit,
     })
 
     const dataCount = await prisma.product.count()
