db7bad91ca20472d2c557ab1be7f7cc1d5d65760
Eslam AbdelWahab <mohaeslam22@gmail.com>
2026-01-29 01:02:03 +0200

    purchase receipt email template

--- email/PurchaseReceipt.tsx
+++ email/PurchaseReceipt.tsx
@@ -1,8 +1,73 @@
+import {Body, Column, Container, Head, Heading, Html, Img, Preview, Row, Section, Tailwind, Text} from "@react-email/components"
 import { Order } from "@/types"
+import { formatterCurrency } from "@/lib/utils"
 
 function PurchaseReceipt({order}: {order:Order}) {
+
+    const dateFormatter = new Intl.DateTimeFormat("en", {dateStyle:"medium"})
+
   return (
-    <div>PurchaseReceipt</div>
+    <Html>
+        <Preview >View order receipt</Preview>
+        <Tailwind>
+            <Head/>
+            <Body className="font-sans bg-white">
+                <Container className="max-w-xl">
+                    <Heading>Purchase Receip</Heading>
+                    <Section>
+                        <Row>
+                            <Column>
+                                <Text className="mb-0 mr-4 text-gray-500 whitespace-nowrap text-nowrap">
+                                    Order Id 
+                                </Text>
+                                <Text className="mt-0 mr-4 ">{order.id.toString()}</Text>
+                            </Column>
+                            <Column>
+                                <Text className="mb-0 mr-4 text-gray-500 whitespace-nowrap text-nowrap">
+                                    Purchase Date 
+                                </Text>
+                                <Text className="mt-0 mr-4 ">{dateFormatter.format(order.createdAt)}</Text>
+                            </Column>
+                            <Column>
+                                <Text className="mb-0 mr-4 text-gray-500 whitespace-nowrap text-nowrap">
+                                    Price Paid 
+                                </Text>
+                                <Text className="mt-0 mr-4 ">{formatterCurrency(order.totalPrice)}</Text>
+                            </Column>
+                        </Row>
+                    </Section>
+                    <Section className="border border-solid border-gray-500 rounded-lg p-4 md:p6 my-4">
+                        {order.orderItem.map(item=> (
+                            <Row key={item.productId} className="mt-8">
+                                <Column className="w-20">
+                                    <Img width="80" alt={item.name} className="rounded" src={item.image.startsWith("/") ? `${process.env.NEXT_PUBLIC_SERVER_URL}${item.image}` : item.image} />
+                                </Column>
+                                <Column className="align-top">
+                                {item.name} x {item.qty}</Column>
+                                <Column align="right" className="align-top">
+                                {formatterCurrency(item.price)}</Column>
+
+                            </Row>
+                        ))}
+                        {[
+                            {name: "Items", price:order.itemsPrice},
+                            {name: "Tax", price:order.taxPrice},
+                            {name: "Shipping", price:order.shippingPrice},
+                            {name: "Total", price:order.totalPrice},
+                        ].map(({name, price})=>(
+                            <Row key={name} className="py-1">
+                                <Column align="right">
+                                {name}:</Column>
+                                <Column align="right" width={70} className="align-top">
+                                <Text className="m-0" >{formatterCurrency(price)}</Text>
+                            </Column>
+                            </Row>
+                        ))}
+                    </Section>
+                </Container>
+            </Body>
+        </Tailwind>
+    </Html>
   )
 }
 
