4950f6960af779e470119301d5e3e378f71205dd
Eslam AbdelWahab <mohaeslam22@gmail.com>
2026-02-01 13:46:26 +0200

    dynamic layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,8 @@
 import Footer from "@/components/Footer"
 import Header from "@/components/shared/header"
 
+export const dynamic = "force-dynamic";
+
 function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
     <div className="flex flex-col h-screen">
